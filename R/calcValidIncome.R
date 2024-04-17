81bb48a447e58530f7a518fff988fde6559e1240
Johannes Koch <jokoch@pik-potsdam.de>
2024-04-17 15:08:07 +0200

    Remove outdated calls to mrdrivers functions and adapt income units

--- R/calcValidIncome.R
+++ R/calcValidIncome.R
@@ -2,16 +2,14 @@
 #'
 #' Returns historical development of income and future projections of income dynamics
 #'
-#'
-#' @param datasource To switch between historical values of James et al (default) or
-#' with future projections for different scenarios (James_OECD_Nakicenovic)
+#' @param datasource "WDI-MI_SSPs-MI"
 #' @return list of magpie object with data and weight. Since intensive and extensive variables
 #' are mixed please keep the mixed_aggregation
 #' @importFrom madrat vcat calcOutput
 #' @importFrom magclass mbind add_dimension getSets getSets<- collapseNames getYears
 #' @author Florian Humpenoeder, Abhijeet Mishra, Kristine Karstens
 
-calcValidIncome <- function(datasource = "James") {
+calcValidIncome <- function(datasource = "WDI-MI_SSPs-MI") {
 
 
   if (datasource == "WDI-MI_SSPs-MI") {
@@ -21,13 +19,13 @@ calcValidIncome <- function(datasource = "James") {
       add_dimension(collapseNames(x), dim = 3.2, add = "variable", nm = nm)
     }
 
-    names <- c("Income (million US$05 MER/yr)",
-               "Income (US$05 MER/cap/yr)",
-               "Income (million US$05 PPP/yr)",
-               "Income (US$05 PPP/cap/yr)")
+    names <- c("Income (million US$17 MER/yr)",
+               "Income (US$17 MER/cap/yr)",
+               "Income (million US$17 PPP/yr)",
+               "Income (US$17 PPP/cap/yr)")
 
-    mer   <- .tmp(calcOutput("GDP", unit = "constant 2005 US$MER", naming = "scenario", aggregate = FALSE), names[1])
-    merpc <- .tmp(calcOutput("GDPpc", unit = "constant 2005 US$MER", naming = "scenario", aggregate = FALSE), names[2])
+    mer   <- .tmp(calcOutput("GDP", unit = "constant 2017 US$MER", naming = "scenario", aggregate = FALSE), names[1])
+    merpc <- .tmp(calcOutput("GDPpc", unit = "constant 2017 US$MER", naming = "scenario", aggregate = FALSE), names[2])
     ppp   <- .tmp(calcOutput("GDP", naming = "scenario", aggregate = FALSE), names[3])
     ppppc <- .tmp(calcOutput("GDPpc", naming = "scenario", aggregate = FALSE), names[4])
 
@@ -42,7 +40,7 @@ calcValidIncome <- function(datasource = "James") {
 
     # Setting weights correctly for intensive and extensive variables
     popWeights <- collapseNames(calcOutput(type = "GDPpc",
-                                           unit = "constant 2005 US$MER",
+                                           unit = "constant 2017 US$MER",
                                            naming = "scenario",
                                            supplementary = TRUE,
                                            aggregate = FALSE)$weight
@@ -60,45 +58,7 @@ calcValidIncome <- function(datasource = "James") {
     weight <- mbind(weight, add_dimension(popWeights, dim = 3.3, add = "variable", nm = names[4]))
 
 
-  } else if (datasource == "James") {
-
-    mer   <- calcOutput("GDPPast", GDPPast = "IHME_USD05_MER_pc", aggregate = FALSE)
-    merpc <- readSource("James", subtype = "IHME_USD05_MER_pc")
-
-    ppp   <- calcOutput("GDPPast", GDPPast = "IHME_USD05_PPP_pc", aggregate = FALSE)
-    ppppc <- readSource("James", subtype = "IHME_USD05_PPP_pc")
-
-    years <- intersect(getYears(merpc), getYears(mer))
-
-    out   <- NULL
-    out   <- mbind(mer[, years, ], merpc[, years, ], ppp[, years, ], ppppc[, years, ])
-
-    names <- c("Income (million US$05 MER/yr)", "Income (US$05 MER/cap/yr)",
-               "Income (million US$05 PPP/yr)", "Income (US$05 PPP/cap/yr)")
-
-    getNames(out, dim = 1) <- names
-    out <- add_dimension(out, dim = 3.1, add = "scenario", nm = "historical")
-    out <- add_dimension(out, dim = 3.2, add = "model", nm = datasource)
-
-
-    # Setting weights correctly for intensive and extensive variables
-    popWeights  <- readSource("WDI", subtype = "SP.POP.TOTL") + 10^-10
-    noWeights   <- popWeights
-    noWeights[] <- 0
-
-    weight <- NULL
-    weight <- mbind(noWeights, popWeights, noWeights, popWeights)
-
-    getNames(weight) <- names
-    weight <- add_dimension(weight, dim = 3.1, add = "scenario", nm = "historical")
-    weight <- add_dimension(weight, dim = 3.2, add = "model", nm = datasource)
-
-    years <- intersect(getYears(out), getYears(weight))
-    out <- out[, years, ]
-    weight <- weight[, years, ]
-
   } else {
-
     vcat(verbosity = 1, paste("Wrong selection of parameters in calcOutput"))
     stop("Given datasource currently not supported!")
   }
@@ -106,7 +66,7 @@ calcValidIncome <- function(datasource = "James") {
   return(list(x = out,
               weight = weight,
               mixed_aggregation = TRUE,
-              unit = "(million) US Dollar 2005 equivalents in MER/yr, MER/cap/yr, PPP/yr, PPP/cap/yr",
+              unit = "(million) US Dollar 2017 equivalents in MER/yr, MER/cap/yr, PPP/yr, PPP/cap/yr",
               description = "Income")
   )
 }
