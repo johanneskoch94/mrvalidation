3287e4f5814601f4b2d7c7fb1e681b5e47e933fc
Johannes Koch <jokoch@pik-potsdam.de>
2024-05-06 17:12:23 +0200

    Switch GDP unit

--- R/calcValidAgriResearchIntensity.R
+++ R/calcValidAgriResearchIntensity.R
@@ -21,7 +21,7 @@ calcValidAgriResearchIntensity<-function(datasource = "Pardey"){
     agRD <- readSource("PardeyAgRD", convert = TRUE)
 
 
-    gdp <- collapseNames(calcOutput("GDP", unit = "constant 2005 US$MER",
+    gdp <- collapseNames(calcOutput("GDP", unit = "constant 2017 US$MER",
                       naming = "scenario", aggregate = FALSE)[,,"SSP2"])
 
     cyears <- intersect(getYears(agRD), getYears(gdp))
